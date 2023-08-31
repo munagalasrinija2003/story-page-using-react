737baddd66e05e9d7110c76c2ae54aa15fd6ceec
munagalasrinija2003 <115577574+munagalasrinija2003@users.noreply.github.com>
2023-08-31 11:44:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,83 @@
+ReactDOM.render(
+    <div>
+        <div id="bor"><center id="boarder"><img src="https://1000logos.net/wp-content/uploads/2018/01/Font-BTS-logo.jpg" alt="" height="100" width="200"></img></center></div>
+<hr></hr>
+<br></br>
+ <center><img src="./bts.jpg" alt="bts" height="700px" width="400px"></img></center>
+        <h3><p id="color">BANGTAN SONYEONDAN</p></h3>
+        <div id="font">
+        <left><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmOsw57mvwarp7QHIboNqQZ77GF6516I21Aw&usqp=CAU" align="left" alt=""height="150" width="100"/></left>
+        <p id="font"><h4><b > BTS is Bangtan Sonyeondan</b><i>also known as “Bulletproof Boy Scouts” or
+       “Bangtan Boys”,is the most famous kpop boy brand group , South Korean K-pop (Korean pop music) band that shot to international stardom in the late 2010s. Its seven members were Jin by name of Kim Seok-Jin; born in  December 4, 1992, Anyang, South Korea. Suga original name Min Yoongi born in March 9, 1993, Buk-gu. J-Hope by name of Jung Ho-Seok, born in February 18, 1994, Kwangju. RM also called Rap Monster, original name Kim Nam-Joon, born in September 12, 1994, Ilsan. Jimin by name of Park Ji-Min. born October 13, 1995, Busan. V original name Kim Tae-Hyung.
+        born in December 30, 1995, Daegu, and Jungkook by name of Jeon Jeong-jungkook; born in September 1, 1997, Busan.</i></h4></p>
+
+        <right><img src="https://media.pitchfork.com/photos/5e53ea3a5d1a220008b9044f/1:1/w_600/Map%20of%20the%20Soul_BTS.jpg" align="right" alt=""height="150" width="100"></img></right>
+       <br></br> <p id="font"> <h4><i >BTS had a fandom called as</i><b> ARMY [“Adorable Representative MC for Youth”]</b><i> and was popular on social media. The band broke numerous records on YouTube—namely, for the most views of a music video within 24 hours of its release, for such 
+singles as “Boy with Luv” (2019), “Dynamite” (2020), and “Butter” (2021).BTS was formed by Bang Si-Hyuk, a successful songwriter and music producer who established his own studio, Big Hit Entertainment, in 2005. In 2010 he began to assemble talent for a group he was calling the Bangtan Sonyeondan (“Bulletproof Boy Scouts”),
+ which would later become better known as BTS. </i></h4></p>
+
+
+
+<img align="left" src="https://upload.wikimedia.org/wikipedia/en/9/9e/Map_of_the_Soul-_7_~_The_Journey_~.jpg" alt=""height="150" width="100"/>
+<br></br> <p  id="font"><h4><i align="right"> The fist member of BTS is jin he is visual and singer, he is Hyung(big brother) of bts ,and second is suga he is the main rapperin bts and also second hyung.next is namjoon ,he is the leader of BTS and main rapper.and jhope is main dancer in and singer and last hynug.now its come to Maknee line
+    mean smallest ones there comes jimin,v,jungkook.and fist manee is jimin is the best dancer and vocalist,second maniee here comes is V also known as kim taehyung ,he is worlds handsome man and vocalist,and finnaly small maknee called as jungkook ,he is multi talented guy,anfd singer,dancer.</i>
+  </h4> </p>
+  <br></br>
+  <br>
+  </br><br></br>
+  </div>
+  <left><h3 id="col">STORIES OF BTS MEMBERS</h3></left>
+  <left><u><h4>KIM NAMJOON</h4></u></left>
+  <left><img src="https://images.news18.com/ibnlive/uploads/2021/08/bts-leader-rm.jpg" align="left"alt=""height="150" width="120"></img></left><p ><b>RM</b>, whose full name is Kim Namjoon, was the first member of BTS and is the band’s leader. RM, whose previous stage name was Rap Monster, is one of the band’s rappers.
+   He started rapping in the sixth grade after he listened to Epik High’s “Fly” for the first time. In 2007, when he was in middle school,
+   RM started rapping under the name Runch Randa, which led him to the rap crew Daenamhyup. 
+   (The crew also consisted of now-known stars like Top Dogg’s KIDOH, 
+   Big Hit Entertainment producer Supreme Boi and Show Me the Money 3 finalist Iron.) 
+   After that, RM started releasing more songs as a solo rapper until he was invited to audition for a 
+   hip-hop agency, where he met Untouchable’s Sleepy, who was a judge at the audition. Impressed by RM’s rapping, Sleepy connected RM to producer Bang Shi Hyuk, who invited him to audition for Big Hit Entertainment. 
+   Then, in 2010, RM became one of BTS band members, and the rest is history.</p>
+   <br></br>
+   <br></br>
+   <left><u><h4>KIM SEOKJIN</h4></u></left>
+<left><img src="https://i.zoomtventertainment.com/media/Jin_11.jpg" align="left"alt=""height="150" width="100"></img></left>
+<br></br><p><b>Jin</b>, whose full name is Kim Seokjin, was the fourth member of BTS. Jin was originally scouted by SM Entertainment while on the street of his hometown in Anyang. But, thinking that the offer was a scam, he never followed up with the company. He was scouted again—this time by Big Hit Entertainment—while as a college student. A scout for Big Hit Entertainment saw Jin as he was leaving a bus and convinced him to audition for the company. As a theatre major with no singing or dancing experience, Jin auditioned to be an actor. However, Big Hit Entertainment had other plans and convinced him to become a trainee for BTS. He is now one of the band’s vocalists. Though Jin never planned to become a K-pop idol, we can’t 
+   imagine him as anything else other than one of BTS members.</p>
+   <br>
+   </br><br></br>
+   
+   <left><u><h4>MIN YOONGI</h4></u></left>
+    <left><img src="https://lh3.googleusercontent.com/-_9LNPehVTe5N16a4vuDQaw89iPvnBBmU77PscgXfrCd7ATuuz2237wXHMIbpaXMDnV1z59zOTw2oMYSgLCSNHKClje45Gxv6A=w960-rj-nu-e365" align="left"alt=""height="150" width="100"></img></left>
+    <p >
+     <b>Suga</b> , whose real name is Min Yoongi, started as a rapper under the name Gloss in high school. It didn’t take long for Suga to make a name for himself as both a rapper and a producer. (He was known at the time for producing and composing beats for local artists, like band D-Town, in his hometown of Daegu.) Soon after, Suga started producing tracks for more well-known artists, like Reflow, who worked with Suga on his song “Who Am I.” Suga auditioned for Big Hit Entertainment after he saw a flyer for the company’s rap competition called “Hit It.” Suga didn’t win the competition, but he did take second place, which was enough for Big Hit Entertainment to sign him as a producer and a trainee. Though he planned on only becoming a producer, Suga was convinced to join BTS in 2010 and became one of the band’s rappers. (He’s joked that founder Bang Shi Hyuk “tricked” him into becoming a BTS member.)
+    </p>
+    <br></br>
+    <br></br>
+    <left><u><h4>JUNG HOSEOK</h4></u></left>
+    <left><img src="https://i.pinimg.com/736x/3f/1f/db/3f1fdbb12d626c08c6688cb93bc31526.jpg" align="left"alt=""height="150" width="130"></img></left>
+    <br></br>
+    <p >Before BTS,<b> J-Hope</b>, whose full name is Jung Hoseok, was best known as a dancer. He started in the dance crew Neuron in his hometown of Gwangju before he auditioned for JYP Entertainment. Though he made it through a few rounds, J-Hope was eventually cut from JYP, which led him to audition for Big Hit Entertainment instead. Because of his dance skills and his sense of rhythm, Big Hit Entertainment saw J-Hope’s potential was a rapper and signed him as a trainee. However, J-Hope’s debut as one of BTS members almost didn’t happen. While he was a trainee, J-Hope and Big Hit Entertainment almost parted ways until RM convinced them both that BTS needed a member like J-Hope. After that, J-Hope stayed with the band and became one of BTS’s three rappers, alongside RM and Suga. And so that’s how all of BTS members were discovered.
+    </p>
+    <left><u><h4>KIM TAEHYUNG</h4></u></left>
+    <left><img src="https://i.pinimg.com/564x/09/3c/6c/093c6ca075081243a5ce89428dfc029e.jpg" align="left"alt=""height="150" width="130"></img></left> 
+    <br></br><p><b>V</b>, whose full name is Kim Taehyung, is the son of farmers in Daegu. With a dream of becoming a musician, V started playing the saxophone in high school, at the recommendation of his father. Soon after, he accompanied a friend to an audition for Big Hit Entertainment in Daegu. V didn’t intend to audition himself and was only there to support his friend, but a team member at Big Entertainment saw him and encouraged him to audition. After he called his father for permission, V auditioned on the spot and became the only person from Daegu that day to make it to the next round. He eventually became one of BTS members and is one of the band’s vocalists.
+    </p>
+    <br>
+    </br>
+    <br></br>
+    <left><u><h4>PARK JIMIN</h4></u></left>
+        <left><img src="https://static.toiimg.com/thumb/msid-89244435,width-1280,resizemode-4/89244435.jpg" align="left"alt=""height="150" width="150"></img></left>
+        <p>
+        <br></br> <b>Jimin</b>, whose full name is Park Jimin, was the final member of BTS. While as a student at the Busan High School of Arts, where he studied under the Department of Dance, a dance teacher, impressed by Jimin’s talent, encouraged him to audition for Big Hit Entertainment. After he passed the audition in his hometown in Busan, Jimin was flown to Seoul, where he became a trainee. As the last member, Jimin’s training was the shortest in the band. He debuted with the rest of BTS members in 2013.
+          </p>
+          <br></br>
+          <br></br> 
+          <br></br>
+          <left><u><h4>JEON JUNGKOOK</h4></u></left>
+
+
+           <left><img src="https://i.pinimg.com/564x/c0/b4/02/c0b402ac53856283b43dbbeaec1c6c78.jpg" align="left"alt=""height="200" width="150"></img></left><p >   
+            <br></br>   <b>Jungkook</b>, whose full name is Jeon Jeongguk, is one of BTS’ singers. He’s also the youngest member of the band, making him BTS’ Golden Maknae. Jungkook, whose nickname is Kookie, was discovered when he auditioned for the third season of Superstar K in his hometown of Busan. He was eliminated in the audition round, but the show gave him enough exposure to be scouted by at least seven agencies, including JYP Entertainment, FNC Entertainment and Starship Entertainment. In the end, though, Jungkook chose Big Hit Entertainment to become a trainee at. Jungkook has said that the reason he chose Big Hit Entertainment over the other agencies was because of RM, who impressed him with his rapping, which made Jungkook want to train and work with him. Long story short, RM is one of the reasons that Jungkook is one of BTS members.
+              </p>
+    </div>
+,document.getElementById("txt")
+)
